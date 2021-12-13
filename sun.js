eb0e1c756c058577be65f45ed8aaa370bd721455
zinoopark <93peace@naver.com>
2021-12-13 10:39:35 +0900

    with_sun

--- sun.js
+++ sun.js
@@ -1,6 +1,19 @@
 export class Sun {
     constructor() {
         this.radius = 200;
+
+        this.total = 60;
+        this.gap = 1 / this.total;
+        this.originPos = [];
+        this.pos = [];
+        for (let i =0; i < this.total; i++) {
+            const pos = this.getCirclePoint( this.radius, this.gap * i);
+            this.originPos[i] = pos;
+            this.pos[i] = pos;
+        }
+
+        this.fps = 30;
+        this.fpsTime = 1000 / this.fps;
     }
 
     resize(stageWidth, stageHeight) {
@@ -12,9 +25,37 @@ export class Sun {
     }
 
     draw(ctx, t) {
-        ctx.fillStyle = 'ffb200';
-        ctx.beginPath();
-        ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+        ctx.fillStyle = '#ffb200';
+        ctx.beginPath(); 
+        let pos = this.pos[0];
+        ctx.moveTo(pos.x + this.x, pos.y + this.y);
+        for (let i =1; i < this.total; i++){
+            const pos = this.pos[i];
+            ctx.lineTo(pos.x + this.x, pos.y + this.y);
+        }
         ctx.fill();
+    }
+
+    updatePoints() {
+        for (let i = 1; i < this.total; i++) {
+            const pos = this.originPos[i];
+            this.pos[i] = {
+                x: pos.x + this.ranInt(5),
+                y: pos.y + this.ranInt(5),
+            }
+        }
+    }
+
+    ranInt(max) {
+        return Math.random() * max;
+    }
+
+    getCirclePoint( radius, t) {
+        const theta = Math.PI * 2 * t;
+
+        return{
+            x: (Math.cos(theta) * radius),
+            y: (Math.sin(theta) * radius)
+        };
     }
 }
